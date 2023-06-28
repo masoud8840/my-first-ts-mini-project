1f1baeae54b76057ae821b8e911943e251830293
masoud8840 <masoudgharedaghi@yahoo.com>
2023-06-28 16:20:02 +0330

    edit some styles

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { setupCounter } from "./counter.ts";
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <section class="container">
     <form class="card todo-form">
-      <input type="text" id="todo-input" />
+      <input type="text" id="todo-input" placeholder="Enter todo name"/>
       <button type="submit">Add This ToDo</button>
   </form>
   </section>  
